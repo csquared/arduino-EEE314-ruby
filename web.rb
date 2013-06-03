316000295c56c8617481488901e54f63527d9684
Chris Continanza <christopher.continanza@gmail.com>
2013-06-03 00:04:13 -0700

    updates

--- web.rb
+++ web.rb
@@ -1,20 +1,25 @@
 require 'sinatra/base'
 
 class Web < Sinatra::Base
+  @@led_state = 'on'
 
   # Show an HTML page that displays the current state
   get '/led' do
-    @led_state = @@led_state || 'unknown'
+    puts "GET led"
+    @led_state = @@led_state
     erb :led
   end
 
   # Return a text body with the state
   get '/led-state' do
+    puts "GET led-state - #{@@led_state}"
     @@led_state
   end
 
   # Set the led state
   post '/led' do
-    @@led_state = request.body.read
+    value = request.body.read
+    puts "POST led #{value}"
+    @@led_state = value
   end
 end
