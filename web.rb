62c5f695ce7c14ecdfcfa6ad8eb5486d0d1d7864
Chris Continanza <christopher.continanza@gmail.com>
2013-06-02 20:17:01 -0700

    simplest web interaction

--- web.rb
+++ web.rb
@@ -1,7 +1,20 @@
 require 'sinatra/base'
 
 class Web < Sinatra::Base
-  get '*' do
-    'hello'
+
+  # Show an HTML page that displays the current state
+  get '/led' do
+    @led_state = @@led_state || 'unknown'
+    erb :led
+  end
+
+  # Return a text body with the state
+  get '/led-state' do
+    @@led_state
+  end
+
+  # Set the led state
+  post '/led' do
+    @@led_state = request.body.read
   end
 end
