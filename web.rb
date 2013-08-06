af3418b08e9d4a94d50b89a398fb9c2aea55d536
Chris Continanza <christopher.continanza@gmail.com>
2013-08-05 21:26:12 -0700

    restify

--- web.rb
+++ web.rb
@@ -1,17 +1,19 @@
 require 'sinatra/base'
 
+$stdout.sync = true
+
 class Web < Sinatra::Base
   @@led_state = 'on'
 
   # Show an HTML page that displays the current state
-  get '/led' do
+  get '/' do
     puts "GET led"
     @led_state = @@led_state
     erb :led
   end
 
   # Return a text body with the state
-  get '/led-state' do
+  get '/led' do
     puts "GET led-state - #{@@led_state}"
     @@led_state
   end
